a83d93aba0d6b99567722c32e4a19d65f9848e0f
Zackautocracy <codeautocracy@gmail.com>
2022-01-18 07:28:32 +0100

    SET-11 declared firebase config in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,21 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire/compat';
 import { SharedModule } from './shared/shared.module';
-import { environment } from 'src/environments/environment';
+// import { environment } from 'src/environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { CoreModule } from './core/core.module';
+import firebase from 'firebase/compat';
+
+//Todo (zack): Investigate why Webpack doesn't bind environment.ts
+const firebase_init = {
+  apiKey: process.env.SE_API_KEY,
+  authDomain: process.env.SE_AUTH_DOMAIN,
+  projectId: process.env.SE_PROJECT_ID,
+  storageBucket: process.env.SE_STORAGE_BUCKET,
+  messagingSenderId: process.env.SE_MESSAGING_SENDER_ID,
+  appId: process.env.SE_APP_ID,
+  measurementId: process.env.SE_MEASUREMENT_ID
+};
 
 @NgModule({
   declarations: [AppComponent],
@@ -17,7 +29,7 @@ import { CoreModule } from './core/core.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireModule.initializeApp(firebase_init),
     AngularFirestoreModule,
     CoreModule
   ],
