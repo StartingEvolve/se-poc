707bcdb4bcf4a63e175b54c7b1c081276138e4c9
Zackautocracy <codeautocracy@gmail.com>
2022-01-18 07:00:02 +0100

    SET-11 migrated to webpack DefinePlugin for compat

--- custom-webpack.config.js
+++ custom-webpack.config.js
@@ -1,18 +1,18 @@
-const { EnvironmentPlugin } = require("webpack");
+const { DefinePlugin } = require("webpack");
 
 // Export a configuration object
 // See [Wepack's documentation](https://webpack.js.org/configuration/) for additional ideas of how to
 // customize your build beyond what Angular provides.
 module.exports = {
   plugins: [
-    new EnvironmentPlugin([
-      'SE_API_KEY',
-      'SE_AUTH_DOMAIN',
-      'SE_PROJECT_ID',
-      'SE_STORAGE_BUCKET',
-      'SE_MESSAGING_SENDER_ID',
-      'SE_APP_ID',
-      'SE_MEASUREMENT_ID'
-    ])
+    new DefinePlugin({
+      'process.env.SE_API_KEY': JSON.stringify(process.env.SE_API_KEY),
+      'process.env.SE_AUTH_DOMAIN': JSON.stringify(process.env.SE_AUTH_DOMAIN),
+      'process.env.SE_PROJECT_ID': JSON.stringify(process.env.SE_PROJECT_ID),
+      'process.env.SE_STORAGE_BUCKET': JSON.stringify(process.env.SE_STORAGE_BUCKET),
+      'process.env.SE_MESSAGING_SENDER_ID': JSON.stringify(process.env.SE_MESSAGING_SENDER_ID),
+      'process.env.SE_APP_ID': JSON.stringify(process.env.SE_APP_ID),
+      'process.env.SE_MEASUREMENT_ID': JSON.stringify(process.env.SE_MEASUREMENT_ID)
+    })
   ]
 };
