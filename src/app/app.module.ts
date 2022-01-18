b28741d857c1fe1bc11fbd517489be751bb7a89b
Zackautocracy <codeautocracy@gmail.com>
2022-01-18 16:19:33 +0100

    SET-11 eliminated explicit env declaration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireModule } from '@angular/fire/compat';
 import { SharedModule } from './shared/shared.module';
-// import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { CoreModule } from './core/core.module';
 import firebase from 'firebase/compat';
-
-//Todo (zack): Investigate why Webpack doesn't bind environment.ts
-const firebase_init = {
-  apiKey: process.env.SE_API_KEY,
-  authDomain: process.env.SE_AUTH_DOMAIN,
-  projectId: process.env.SE_PROJECT_ID,
-  storageBucket: process.env.SE_STORAGE_BUCKET,
-  messagingSenderId: process.env.SE_MESSAGING_SENDER_ID,
-  appId: process.env.SE_APP_ID,
-  measurementId: process.env.SE_MEASUREMENT_ID
-};
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,7 +18,7 @@ const firebase_init = {
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
-    AngularFireModule.initializeApp(firebase_init),
+    AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     CoreModule
   ],
