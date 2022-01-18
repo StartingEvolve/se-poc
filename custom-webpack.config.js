7dc5f2a089157323fae77ca66fea6aa19944b652
Zackautocracy <codeautocracy@gmail.com>
2022-01-18 05:32:39 +0100

    SET-11 added custom webpack to bind env vars

---
+++ custom-webpack.config.js
@@ -0,0 +1,18 @@
+const { EnvironmentPlugin } = require("webpack");
+
+// Export a configuration object
+// See [Wepack's documentation](https://webpack.js.org/configuration/) for additional ideas of how to
+// customize your build beyond what Angular provides.
+module.exports = {
+  plugins: [
+    new EnvironmentPlugin([
+      SE_API_KEY,
+      SE_AUTH_DOMAIN,
+      SE_PROJECT_ID,
+      SE_STORAGE_BUCKET,
+      SE_MESSAGING_SENDER_ID,
+      SE_APP_ID,
+      SE_MEASUREMENT_ID
+    ])
+  ]
+};
