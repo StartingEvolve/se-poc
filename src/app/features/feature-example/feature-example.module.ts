545c977b66d4bf4900dbecaaf3111dd4daff7a68
bobbhy <saaderraz99@gmail.com>
2022-01-21 18:06:47 +0100

    Fix merge conflicts

--- src/app/features/feature-example/feature-example.module.ts
+++ src/app/features/feature-example/feature-example.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { FeatureExampleRoutingModule } from './feature-example-routing.module';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { SharedModule } from '@shared/shared.module';
 import { TestComponent } from './views/test/test.component';
 
 @NgModule({
