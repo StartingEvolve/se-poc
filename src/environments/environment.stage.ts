2afa97af1af9833e3e4ddafe884ba1d5e5b720bd
Zackautocracy <codeautocracy@gmail.com>
2022-01-31 00:24:48 +0100

    SET-70 changed env vars in staging

--- src/environments/environment.stage.ts
+++ src/environments/environment.stage.ts
@@ -2,21 +2,12 @@ export const environment = {
   production: true,
   useEmulators: false,
   firebase: {
-    apiKey: 'AIzaSyC3aZjZB-XU6OE39sdfQXY7MMCydj6MXBU',
-    authDomain: 'angular-fire-e40b7.firebaseapp.com',
-    projectId: 'angular-fire-e40b7',
-    storageBucket: 'angular-fire-e40b7.appspot.com',
-    messagingSenderId: '71305995040',
-    appId: '1:71305995040:web:d40f052ff72ad77f99ed45',
-    measurementId: 'G-TKQGYQVF4S'
+    apiKey: process.env.SE_API_KEY,
+    authDomain: process.env.SE_AUTH_DOMAIN,
+    projectId: process.env.SE_PROJECT_ID,
+    storageBucket: process.env.SE_STORAGE_BUCKET,
+    messagingSenderId: process.env.SE_MESSAGING_SENDER_ID,
+    appId: process.env.SE_APP_ID,
+    measurementId: process.env.SE_MEASUREMENT_ID
   }
-  // firebase: {
-  //   apiKey: process.env.SE_API_KEY,
-  //   authDomain: process.env.SE_AUTH_DOMAIN,
-  //   projectId: process.env.SE_PROJECT_ID,
-  //   storageBucket: process.env.SE_STORAGE_BUCKET,
-  //   messagingSenderId: process.env.SE_MESSAGING_SENDER_ID,
-  //   appId: process.env.SE_APP_ID,
-  //   measurementId: process.env.SE_MEASUREMENT_ID
-  // }
 };
