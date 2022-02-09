e7f9e069bf2a3ca8087cf5edbc4193b9808f56fe
Zackautocracy <codeautocracy@gmail.com>
2022-02-09 02:55:51 +0100

    SET-79 added final carousel changes to landing page

--- src/app/shared/components/carousel/carousel.component.ts
+++ src/app/shared/components/carousel/carousel.component.ts
@@ -1,94 +1,167 @@
 import {
   Component,
+  Input,
+  Inject,
+  OnInit,
+  OnDestroy,
   ViewChild,
   ElementRef,
-  AfterViewInit,
-  Input
+  AfterViewInit
 } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
+import TinySliderConfig from '@vendors/tiny-slider/tiny-slider.config';
+import { fromEvent, timer } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
+export interface Script {
+  scriptUrl: string;
+  isLoaded: boolean;
+}
+
+export interface ThirdPartyLibrary {
+  scripts: string[];
+  styles: string[];
+}
 
 @Component({
   selector: 'se-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent implements AfterViewInit {
-  @ViewChild('CarouselInner') CarouselInner: ElementRef;
+//Todo (zack): Refactor the component behavior into a webworker & create a scripts service
+export class CarouselComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() carouselItems: any[];
-  constructor() {
-    this.carouselItems;
+  @ViewChild('CarouselInner') carouselInnerElem: ElementRef;
+  carouselItemId: string;
+
+  tinySliderConfig: any;
+  scripts: Script[];
+  libConfig: ThirdPartyLibrary;
+
+  //Todo (zack) : Load component dynamically with view containers
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router
+  ) {
+    this.libConfig = {
+      scripts: ['../../assets/vendors/tiny-slider/tiny-slider.js'],
+      styles: ['../../assets/vendors/tiny-slider/tiny-slider.min.css']
+    };
+  }
+
+  setCarouselItemId(itemId: string) {
+    this.carouselItemId = itemId;
+  }
+
+  ngOnInit() {
+    this.loadAPI();
   }
 
   ngAfterViewInit() {
-    this.onResize();
-  }
-  onResize() {
-    setTimeout(() => {
-      this.toggleCarouselBtns();
-    }, 200);
+    const mousedown$ = fromEvent(
+      this.carouselInnerElem.nativeElement,
+      'mousedown'
+    );
+
+    //Ignore drag events that trigger a click
+    const mouseup$ = fromEvent(this.carouselInnerElem.nativeElement, 'mouseup');
+
+    mousedown$.subscribe(() => {
+      const clickTimer$ = timer(100);
+      mouseup$
+        .pipe(takeUntil(clickTimer$))
+        .subscribe((e) => this.navigateToArticle(this.carouselItemId));
+    });
   }
 
-  onCarouselArrowClick(direction: any) {
-    let carouselOffsetWidth = this.CarouselInner.nativeElement.offsetWidth;
-    let carouselItemWIdth =
-      document.querySelector('.carousel-item').clientWidth;
-    let carouselScrollWidth = this.CarouselInner.nativeElement.scrollWidth;
-    if (direction == 'left')
-      this.CarouselInner.nativeElement.scrollTo({
-        left:
-          this.CarouselInner.nativeElement.scrollLeft -
-          carouselOffsetWidth +
-          carouselItemWIdth,
-        behavior: 'smooth'
+  //Todo (zack) : Separate mouse clicks with drag events
+  navigateToArticle(articleId: string) {
+    this.router.navigate(['article', articleId]);
+  }
+
+  loadBulkScripts(scriptUrls: string[]) {
+    if (scriptUrls) {
+      const promises: any[] = [];
+      this.scripts = scriptUrls.map((script) => {
+        return {
+          scriptUrl: script,
+          isLoaded: false
+        };
       });
-    else
-      this.CarouselInner.nativeElement.scrollTo({
-        left:
-          this.CarouselInner.nativeElement.scrollLeft +
-          carouselOffsetWidth -
-          carouselItemWIdth,
-        behavior: 'smooth'
-      });
-
-    setTimeout(() => {
-      this.toggleCarouselBtns();
-    }, 500);
-  }
-
-  toggleCarouselBtns() {
-    let carouselScrollWidth = this.CarouselInner.nativeElement.scrollWidth;
-    let carouselOffsetWidth = this.CarouselInner.nativeElement.offsetWidth;
-    let scrollLeft = this.CarouselInner.nativeElement.scrollLeft;
-    let carouselNextBtn: HTMLElement =
-      document.getElementById('carousel-next-btn');
-    let carouselPrevBtn: HTMLElement =
-      document.getElementById('carousel-prev-btn');
-
-    if (scrollLeft > 5) carouselPrevBtn.classList.remove('d-none');
-    else carouselPrevBtn.classList.add('d-none');
-
-    if (carouselOffsetWidth + scrollLeft + 15 >= carouselScrollWidth)
-      carouselNextBtn.classList.add('d-none');
-    else carouselNextBtn.classList.remove('d-none');
-  }
-
-  onSwipe(swipe: any) {
-    //swipe-right = 4 & swipe-left = 2
-    console.log(swipe);
-    if (swipe.direction == 4) {
-      this.CarouselInner.nativeElement.scrollTo({
-        left: this.CarouselInner.nativeElement.scrollLeft - swipe.distance,
-        behavior: 'smooth'
-      });
+      console.log(this.scripts);
+      this.scripts.forEach((script) =>
+        promises.push(this.loadScript(script.scriptUrl))
+      );
+      return Promise.all(promises);
     }
-    if (swipe.direction == 2) {
-      this.CarouselInner.nativeElement.scrollTo({
-        left: this.CarouselInner.nativeElement.scrollLeft + swipe.distance,
-        behavior: 'smooth'
-      });
+    return null;
+  }
+
+  //Todo (zack): find an interface between local dynamic scripts and local configuration
+  //i.e : I want to be able to interact with the slider object from within the component
+  //If it ends up adding substantial accidental complexity, I'll create a wrapper for API calls between the script and the component
+
+  loadScript(scriptUrl: string) {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = scriptUrl;
+      script.type = 'text/javascript';
+      script.async = false;
+      //Avoid race conditions in Window global variables
+      script.onload = () => {
+        for (let s of this.scripts) {
+          if (s.scriptUrl === scriptUrl) {
+            s.isLoaded = true;
+            resolve({ script: scriptUrl, loaded: true });
+          }
+        }
+      };
+      script.onerror = () => reject({ scriptUrl, isLoaded: false });
+      document.getElementsByTagName('head')[0].appendChild(script);
+    });
+  }
+
+  loadConfig() {
+    this.tinySliderConfig = new TinySliderConfig();
+  }
+
+  loadStyle(styleUrls: string[]) {
+    if (styleUrls) {
+      const head = this.document.getElementsByTagName('head')[0];
+
+      for (let i of styleUrls) {
+        const style = this.document.createElement('link');
+        style.id = 'article-carousel';
+        style.rel = 'stylesheet';
+        style.href = i;
+        head.appendChild(style);
+      }
     }
-    console.log(swipe.direction);
-    setTimeout(() => {
-      this.toggleCarouselBtns();
-    }, 500);
+  }
+
+  loadAPI() {
+    this.loadStyle(this.libConfig.styles);
+    this.loadBulkScripts(this.libConfig.scripts).then(() => this.loadConfig());
+  }
+
+  //Dynamically loaded scripts are discarded on every route navigation
+  // reloadScripts() {
+  //   this.routerEvent$ = this.router.events
+  //     .pipe(filter((event) => event instanceof NavigationEnd))
+  //     .subscribe((event: NavigationEnd) => {
+  //       if (event.url === this.componentRoute) {
+  //         // this.router.navigate([this.componentRoute]);
+  //         console.log(event);
+  //       }
+  //     });
+  // }
+
+  //Todo: Clear the scripts on destroy
+  ngOnDestroy() {
+    // if (this.routerEvent$) {
+    //   this.routerEvent$.unsubscribe();
+    // }
+    this.tinySliderConfig.destroy();
   }
 }
