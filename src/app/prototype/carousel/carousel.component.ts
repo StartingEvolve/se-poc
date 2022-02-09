b66a981d8678ac7228bc7d42f0511802f44d8bc2
Zackautocracy <codeautocracy@gmail.com>
2022-02-09 02:26:33 +0100

    SET-79 fixed scripts lifecycle on routes navigations

--- src/app/prototype/carousel/carousel.component.ts
+++ src/app/prototype/carousel/carousel.component.ts
@@ -1,16 +1,39 @@
-import { Component, Input, Inject } from '@angular/core';
+import { Component, Input, Inject, OnInit, OnDestroy } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
+import { Router } from '@angular/router';
+import TinySliderConfig from '@vendors/tiny-slider/tiny-slider.config';
+
+export interface Script {
+  scriptUrl: string;
+  isLoaded: boolean;
+}
+
+export interface ThirdPartyLibrary {
+  scripts: string[];
+  styles: string[];
+}
 
 @Component({
   selector: 'se-carousel',
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.scss']
 })
-export class CarouselComponent {
+//Todo (zack): Refactor the component behavior into a webworker & create a scripts service
+export class CarouselComponent implements OnInit, OnDestroy {
   @Input() carouselItems: any[];
+  tinySliderConfig: any;
+  scripts: Script[];
+  libConfig: ThirdPartyLibrary;
 
-  //Todo (zack) : sync script loading with rendering without relying on the constructor
-  constructor(@Inject(DOCUMENT) private document: Document) {
+  //Todo (zack) : Load component dynamically with view containers
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private router: Router
+  ) {
+    this.libConfig = {
+      scripts: ['../../assets/vendors/tiny-slider/tiny-slider.js'],
+      styles: ['../../assets/vendors/tiny-slider/tiny-slider.min.css']
+    };
     this.carouselItems = [
       {
         title: 'Datamining avec python',
@@ -76,39 +99,99 @@ export class CarouselComponent {
         articleId: ''
       }
     ];
-    this.loadStyle();
-    this.loadScripts();
+  }
+
+  ngOnInit() {
+    this.loadAPI();
   }
 
   //Todo (zack) : Separate mouse clicks with drag events
+  navigateToArticle(articleId: string) {
+    this.router.navigate(['article', articleId]);
+  }
+
+  loadBulkScripts(scriptUrls: string[]) {
+    if (scriptUrls) {
+      const promises: any[] = [];
+      this.scripts = scriptUrls.map((script) => {
+        return {
+          scriptUrl: script,
+          isLoaded: false
+        };
+      });
+      console.log(this.scripts);
+      this.scripts.forEach((script) =>
+        promises.push(this.loadScript(script.scriptUrl))
+      );
+      return Promise.all(promises);
+    }
+    return null;
+  }
 
   //Todo (zack): find an interface between local dynamic scripts and local configuration
   //i.e : I want to be able to interact with the slider object from within the component
   //If it ends up adding substantial accidental complexity, I'll create a wrapper for API calls between the script and the component
-  loadScripts() {
-    // This array contains all the files/CDNs
-    const dynamicScripts = [
-      '../../assets/vendors/tiny-slider/tiny-slider.js',
-      '../../assets/vendors/tiny-slider/tiny-slider.config.js'
-    ];
-    for (let i of dynamicScripts) {
-      const node = document.createElement('script');
-      node.src = i;
-      node.type = 'text/javascript';
-      node.async = false;
-      document.getElementsByTagName('head')[0].appendChild(node);
+
+  loadScript(scriptUrl: string) {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement('script');
+      script.src = scriptUrl;
+      script.type = 'text/javascript';
+      script.async = false;
+      //Avoid race conditions in Window global variables
+      script.onload = () => {
+        for (let s of this.scripts) {
+          if (s.scriptUrl === scriptUrl) {
+            s.isLoaded = true;
+            resolve({ script: scriptUrl, loaded: true });
+          }
+        }
+      };
+      script.onerror = () => reject({ scriptUrl, isLoaded: false });
+      document.getElementsByTagName('head')[0].appendChild(script);
+    });
+  }
+
+  loadConfig() {
+    this.tinySliderConfig = new TinySliderConfig();
+  }
+
+  loadStyle(styleUrls: string[]) {
+    if (styleUrls) {
+      const head = this.document.getElementsByTagName('head')[0];
+
+      for (let i of styleUrls) {
+        const style = this.document.createElement('link');
+        style.id = 'article-carousel';
+        style.rel = 'stylesheet';
+        style.href = i;
+        head.appendChild(style);
+      }
     }
   }
 
-  loadStyle() {
-    const head = this.document.getElementsByTagName('head')[0];
-
-    const style = this.document.createElement('link');
-    style.id = 'article-carousel';
-    style.rel = 'stylesheet';
-    style.href = '../../assets/vendors/tiny-slider/tiny-slider.min.css';
-
-    head.appendChild(style);
+  loadAPI() {
+    this.loadStyle(this.libConfig.styles);
+    this.loadBulkScripts(this.libConfig.scripts).then(() => this.loadConfig());
   }
+
+  //Dynamically loaded scripts are discarded on every route navigation
+  // reloadScripts() {
+  //   this.routerEvent$ = this.router.events
+  //     .pipe(filter((event) => event instanceof NavigationEnd))
+  //     .subscribe((event: NavigationEnd) => {
+  //       if (event.url === this.componentRoute) {
+  //         // this.router.navigate([this.componentRoute]);
+  //         console.log(event);
+  //       }
+  //     });
+  // }
+
   //Todo: Clear the scripts on destroy
+  ngOnDestroy() {
+    // if (this.routerEvent$) {
+    //   this.routerEvent$.unsubscribe();
+    // }
+    this.tinySliderConfig.destroy();
+  }
 }
--- src/assets/vendors/tiny-slider/tiny-slider.config.js
+++ src/assets/vendors/tiny-slider/tiny-slider.config.js
@@ -1,19 +1,31 @@
-let slider = tns({
-  container: '.article-slider',
-  items: 2,
-  slideBy: 1,
-  controlsContainer: '#carousel-controls',
-  mouseDrag: true,
-  nav: false,
-  autoplayButtonOutput: false,
-  swipeAngle: false,
-  responsive: {
-    350: {
-      items: 1,
-      edgePadding: 30
-    },
-    500: {
-      items: 3
-    }
+export default class TinySliderConfig {
+  constructor() {
+    this.slider = this.init();
   }
-});
+
+  init() {
+    return window.tns({
+      container: '.article-slider',
+      items: 2,
+      slideBy: 1,
+      controlsContainer: '#carousel-controls',
+      mouseDrag: true,
+      nav: false,
+      autoplayButtonOutput: false,
+      swipeAngle: false,
+      responsive: {
+        350: {
+          items: 1,
+          edgePadding: 30
+        },
+        500: {
+          items: 3
+        }
+      }
+    });
+  }
+
+  destroy() {
+    this.slider.destroy();
+  }
+}
