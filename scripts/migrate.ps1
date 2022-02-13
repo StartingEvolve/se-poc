96ea7d425c0003ecd44d6483e2277622ee5399e5
Zackautocracy <codeautocracy@gmail.com>
2022-02-13 19:09:09 +0100

    SET-73 added auth user migrations for emulator

--- scripts/migrate.ps1
+++ scripts/migrate.ps1
@@ -25,6 +25,15 @@ function ExportStageFirebaseToEmulator()
   {
     New-Item -ItemType Directory -Path "migrations"
   }
+  If (!(test-path "migrations/auth_export"))
+  {
+    New-Item -ItemType Directory -Path "migrations/auth_export"
+  }
+  If (!(test-path -path "migrations/firebase-export-metadata.json" -PathType Leaf))
+  {
+    Copy-Item "firebase-export-metadata.json" -Destination "migrations"
+  }
+  firebase auth:export migrations/auth_export/accounts.json
   gsutil -m cp -r gs://set-database-migration/v1 migrations
 
 }
