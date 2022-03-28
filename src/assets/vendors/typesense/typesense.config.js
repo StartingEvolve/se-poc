d8b2181cf652aafcb28859c3598b6e113dd48b21
Zackautocracy <codeautocracy@gmail.com>
2022-03-28 14:15:44 +0200

    SET-132 Added self hosted typesense config

--- src/assets/vendors/typesense/typesense.config.js
+++ src/assets/vendors/typesense/typesense.config.js
@@ -1,30 +1,32 @@
 export default class TypesenseConfig {
-    constructor() {
-        this.client = this.init();
-    }
+  constructor() {
+    this.client = this.init();
+  }
 
-    init() {
-        return new window.Typesense.Client({
-            'nodes': [{
-                'host': 'ulz2oqrx4ist0bayp-1.a1.typesense.net', // where xxx is the ClusterID of your Typesense Cloud cluster
-                'port': '443',
-                'protocol': 'https'
-            }],
-            'apiKey': 'VDmo2Lkl9YzuFuPbhRcHpRZhOeSwWzr5',
-            'connectionTimeoutSeconds': 10,
-            'retryIntervalSeconds': 0.1,
-            'healthcheckIntervalSeconds': 2
-        })
-    }
-    getClient() {
-        return this.client
-    }
+  init() {
+    return new window.Typesense.Client({
+      nodes: [
+        {
+          host: 'search.startingevolve.tech', // where xxx is the ClusterID of your Typesense Cloud cluster
+          port: '443',
+          protocol: 'https'
+        }
+      ],
+      apiKey: '8SCbUB6OwcorefYh114fsK1PtTn9OO50',
+      connectionTimeoutSeconds: 10,
+      retryIntervalSeconds: 0.1,
+      healthcheckIntervalSeconds: 2
+    });
+  }
+  getClient() {
+    return this.client;
+  }
 
-    destroy() {
-        this.client.destroy();
-    }
+  destroy() {
+    this.client.destroy();
+  }
 
-    rebuild() {
-        return this.client.rebuild();
-    }
+  rebuild() {
+    return this.client.rebuild();
+  }
 }
